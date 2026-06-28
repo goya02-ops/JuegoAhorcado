0bbb3de2a4546f1fc67707945eb07953efa43051
Bruno Iglina <129758494+BrunoIglina@users.noreply.github.com>
2026-06-28 10:55:26 -0300

    REFACTOR: Separar tests de AT4 y AT5 en sus propios bloques describe

--- tests/Ahorcado.test.ts
+++ tests/Ahorcado.test.ts
@@ -62,6 +62,17 @@ describe("Pruebas unitarias del Acceptance Test 4", () => {
     const juego = new Ahorcado("GATO");
     expect(juego.estasGanado()).toBe(false);
   });
+  it("al adivinar todas las letras, el juego esta ganado", () => {
+    const juego = new Ahorcado("GATO");
+    juego.adivinar("G");
+    juego.adivinar("A");
+    juego.adivinar("T");
+    juego.adivinar("O");
+    expect(juego.estasGanado()).toBe(true);
+  });
+});
+
+describe("Pruebas unitarias del Acceptance Test 5", () => {
   it("al perder, se revela la palabra completa", () => {
     const juego = new Ahorcado("GATO");
     juego.adivinar("E");
