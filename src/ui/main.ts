aaf1e14819922bd6a581ea95eef6ef12350f26bd
Bruno Iglina <129758494+BrunoIglina@users.noreply.github.com>
2026-06-27 21:50:44 -0300

    GREEN: Cablear UI para deshabilitar input y mostrar perder

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -26,6 +26,12 @@ export function mountApp(container: HTMLElement, juego: Ahorcado): void {
     if (juego.estasGanado()) {
       messageEl.textContent = "Ganaste";
       input.disabled = true;
+    } else if (juego.estasPerdido()) {
+      messageEl.textContent = "Perdiste";
+      input.disabled = true;
+    } else {
+      messageEl.textContent = "";
+      input.disabled = false;
     }
   }
 
